754023281f5e1cc23faf7872ae89a4cde3b4eac7
prashant-shekhar <toprashantshekhar@gmail.com>
2021-01-07 16:30:52 +0530

    Finish Static Pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
   get 'static_page/home'
   get 'static_page/help'
+  get 'static_page/about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- test/controllers/static_page_controller_test.rb
+++ test/controllers/static_page_controller_test.rb
@@ -1,6 +1,10 @@
 require "test_helper"
 
 class StaticPageControllerTest < ActionDispatch::IntegrationTest
+  test "should get root" do
+    get static_page_home_url
+    assert_response :success
+  end
   test "should get home" do
     get static_page_home_url
     assert_response :success
@@ -8,6 +12,11 @@ class StaticPageControllerTest < ActionDispatch::IntegrationTest
 
   test "should get help" do
     get static_page_help_url
+    assert_response :success
+  end
+
+  test "should get about" do
+    get static_page_about_url
     assert_response :success
   end
 end
